6607f11d59b5fb9821e04f742bc30db34814016d
ruuk <xbmc@2ndmind.net>
2014-05-26 15:43:49 -0700

    Have osxsay use old class for server

--- osxsay.py
+++ osxsay.py
@@ -14,6 +14,14 @@ class OSXSayTTSBackend(ThreadedTTSBackend):
 					'speed':0
 	}
 	
+	def __new__(cls):
+		try:
+			import xbmc #analysis:ignore
+			return cls()
+		except:
+			pass
+		return OSXSayTTSBackend_SubProcess()
+
 	def __init__(self):
 		import cocoapy
 		self.cocoapy = cocoapy
@@ -82,36 +90,37 @@ class OSXSayTTSBackend(ThreadedTTSBackend):
 		return sys.platform == 'darwin' and not util.isATV2()
 
 #OLD
-#class OSXSayTTSBackend(ThreadedTTSBackend):
-#	provider = 'OSXSay'
-#	displayName = 'OSX Say (OSX Internal)'
-#	canStreamWav = True
-#	interval = 100
-#	
-#	def __init__(self):
-#		self.process = None
-#		self.threadedInit()
-#		
-#	def threadedSay(self,text):
-#		if not text: return
-#		self.process = subprocess.Popen(['say', text.encode('utf-8')])
-#		while self.process.poll() == None and self.active: util.sleep(10)
-#		
-#	def getWavStream(self,text):
-#		wav_path = os.path.join(util.getTmpfs(),'speech.wav')
-#		subprocess.call(['say', '-o', wav_path,'--file-format','WAVE','--data-format','LEI16@22050',text.encode('utf-8')])
-#		return open(wav_path,'rb')
-#		
-#	def isSpeaking(self):
-#		return (self.process and self.process.poll() == None) or ThreadedTTSBackend.isSpeaking(self)
-#
-#	def stop(self):
-#		if not self.process: return
-#		try:
-#			self.process.terminate()
-#		except:
-#			pass
-#
-#	@staticmethod
-#	def available():
-#		return sys.platform == 'darwin' and not util.isATV2()
+class OSXSayTTSBackend_SubProcess(ThreadedTTSBackend):
+	provider = 'OSXSay'
+	displayName = 'OSX Say (OSX Internal)'
+	canStreamWav = True
+	interval = 100
+	
+	def __init__(self):
+		util.LOG('OSXSay using subprocess method class')
+		self.process = None
+		self.threadedInit()
+		
+	def threadedSay(self,text):
+		if not text: return
+		self.process = subprocess.Popen(['say', text.encode('utf-8')])
+		while self.process.poll() == None and self.active: util.sleep(10)
+		
+	def getWavStream(self,text):
+		wav_path = os.path.join(util.getTmpfs(),'speech.wav')
+		subprocess.call(['say', '-o', wav_path,'--file-format','WAVE','--data-format','LEI16@22050',text.encode('utf-8')])
+		return open(wav_path,'rb')
+		
+	def isSpeaking(self):
+		return (self.process and self.process.poll() == None) or ThreadedTTSBackend.isSpeaking(self)
+
+	def stop(self):
+		if not self.process: return
+		try:
+			self.process.terminate()
+		except:
+			pass
+
+	@staticmethod
+	def available():
+		return sys.platform == 'darwin' and not util.isATV2()
